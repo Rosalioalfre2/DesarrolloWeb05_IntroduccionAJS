eadf0b9fa591ebca8e12c0b850271e61461dc757
Rosalioalfre2 <rosalioalfre2@gmail.com>
2021-12-26 17:13:25 -0600

    Finalizando

---
+++ js/26.js
@@ -0,0 +1,23 @@
+// This
+const reservacion = {
+    nombre: 'Rosalio',
+    apellido: 'Monterrosa',
+    total: 5000,
+    pagado: false,
+    informacion: function() {
+        console.log(`El Cliente ${this.nombre} reservó y su cantidad a pagar es de ${this.total}`);
+    }
+}
+
+const reservacion2 = {
+    nombre: 'Fredie',
+    apellido: 'Mercury',
+    total: 5000,
+    pagado: false,
+    informacion: function() {
+        console.log(`El Cliente ${this.nombre} reservó y su cantidad a pagar es de ${this.total}`);
+    }
+}
+
+reservacion.informacion();
+reservacion2.informacion();
